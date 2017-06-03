f40bb01ad41f65f4907d6c3264745e77f00cde41
Jakub Semik <kuba.semik@gmail.com>
2017-06-03 19:52:38 +0200

    New fuckingggg idea

--- src/chat/bindings.py
+++ src/chat/bindings.py
@@ -1,5 +1,8 @@
 from channels_api.bindings import ResourceBinding
 
+from oauth2_provider.models import AccessToken
+from accounts.models import User
+
 from .models import Message
 from .serializers import MessageSerializer
 
@@ -9,3 +12,15 @@ class MessageBinding(ResourceBinding):
     stream = 'messages'
     serializer_class = MessageSerializer
     queryset = Message.objects.all()
+
+    @classmethod
+    def trigger_inbound(cls, message, **kwargs):
+        """
+        Triggers the binding to see if it will do something.
+        Also acts as a consumer.
+        """
+        if message.channel.name == 'websocket.connect':
+            message.user = User.objects.all()[0]
+        return super().trigger_inbound(message, **kwargs)
+
+
--- src/games/models.py
+++ src/games/models.py
@@ -11,24 +11,24 @@ class GameAccount(models.Model):
 
 class LeagueOfLegendsAccount(GameAccount):
     LEAGUE_CHOICES = (
-        ('Bronze', _('Bronze')),
-        ('Silver', _('Silver')),
-        ('Gold', _('Gold')),
-        ('Platinum', _('Platinum')),
-        ('Diamond', _('Diamond')),
-        ('Master', _('Master')),
-        ('Challenger', _('Challenger'))
+        (1, _('Bronze')),
+        (2, _('Silver')),
+        (3, _('Gold')),
+        (4, _('Platinum')),
+        (5, _('Diamond')),
+        (6, _('Master')),
+        (7, _('Challenger'))
     )
     DIVISION_CHOICES = (
-        ('I', 'I'),
-        ('II', 'II'),
-        ('III', 'III'),
-        ('IV', 'IV'),
-        ('V', 'V'),
+        (1, 'I'),
+        (2, 'II'),
+        (3, 'III'),
+        (4, 'IV'),
+        (5, 'V'),
     )
 
-    league = models.TextField(_('User\'s league'), choices=LEAGUE_CHOICES)
-    division = models.TextField(_('User\'s division'), choices=DIVISION_CHOICES)
+    league = models.PositiveIntegerField(_('User\'s league'), choices=LEAGUE_CHOICES)
+    division = models.PositiveIntegerField(_('User\'s division'), choices=DIVISION_CHOICES)
 
     class Meta:
         verbose_name = 'League of Legends account'
