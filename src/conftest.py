69947ce984eae8516edc5c7967b5539dde4d6ebd
Jakub Semik <kuba.semik@gmail.com>
2017-06-07 14:36:04 +0200

    Added missing fixture

--- src/conftest.py
+++ src/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from oauth2_provider.models import Application
 
 from common.generators import generate_user
 
@@ -8,6 +9,13 @@ def normal_user():
     return generate_user()
 
 
-@pytest.fixture
-def admin_user():
-    return generate_user(is_superuser=True)
+@pytest.fixture(autouse=True)
+def application(admin_user):
+    application = Application.objects.create(
+        client_type=Application.CLIENT_CONFIDENTIAL,
+        authorization_grant_type=Application.GRANT_AUTHORIZATION_CODE,
+        redirect_uris='https://localhost/oauth2/callback',
+        name='test',
+        user=admin_user
+    )
+    return application
