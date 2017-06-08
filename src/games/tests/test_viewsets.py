a5ce3da53b15a4599bb0d6b623a8f80bba54f37c
Radek Krzak <radek.krzak@gmail.com>
2017-06-08 10:06:18 +0200

    SingleAccountCreator helper class added, league_acc fixture refactored

--- src/games/tests/test_viewsets.py
+++ src/games/tests/test_viewsets.py
@@ -17,28 +17,6 @@ class BaseTestAccountViewSet(BaseViewTest):
     def new_acc_data(self):
         return self.gen_func()
 
-    @pytest.fixture
-    def _new_instance(self) -> dict:
-        d = {}
-        acc_data = self.gen_func()
-        instance = self.model.objects.create(**acc_data)
-        d['acc_data'] = acc_data
-        d['instance'] = instance
-        d['detail_url'] = reverse(f'{self.view_name}-detail', kwargs={'pk': instance.pk})
-        return d
-
-    @pytest.fixture
-    def existing_acc_data(self, _new_instance: dict):
-        return _new_instance['acc_data']
-
-    @pytest.fixture
-    def instance(self, _new_instance: dict):
-        return _new_instance['instance']
-
-    @pytest.fixture
-    def detail_url(self, _new_instance: dict):
-        return _new_instance['detail_url']
-
     def test_can_create_account(self, client: Client, normal_user_auth, list_url, new_acc_data):
         """
         POST /api/games/<game>/
@@ -47,20 +25,22 @@ class BaseTestAccountViewSet(BaseViewTest):
         response = client.post(list_url, new_acc_data, **normal_user_auth)
         assert response.status_code == 201
 
-    def test_can_retrieve_account(self, client: Client, normal_user_auth, detail_url):
+    def test_can_retrieve_account(self, client: Client, normal_user_auth, league_acc):
         """
         GET /api/games/<game>/{id}/
         Testing retrieving game account as a normal user.
         """
-        response = client.get(detail_url, **normal_user_auth)
+        print(league_acc.detail_url)
+        response = client.get(league_acc.detail_url, **normal_user_auth)
         assert response.status_code == 200
 
-    def test_can_delete_account(self, client: Client, normal_user_auth, detail_url):
+    def test_can_delete_account(self, client: Client, normal_user_auth, league_acc):
         """
         DELETE /api/games/<game>/{id}/
         Testing deleting game account details as a normal user.
         """
-        response = client.delete(detail_url, **normal_user_auth)
+        print(league_acc.detail_url)
+        response = client.delete(league_acc.detail_url, **normal_user_auth)
         assert response.status_code == 204
 
 
@@ -70,12 +50,12 @@ class TestLeagueOfLegendsAccountViewSet(BaseTestAccountViewSet):
     model = LeagueOfLegendsAccount
 
     def test_cannot_create_duplicate_account(self, client: Client, normal_user_auth, list_url,
-                                             existing_acc_data):
+                                             league_acc):
         """
         POST /api/games/league/
-        Ensure we cannot create a duplicate of an account | normal user.
+        Ensure we cannot create a duplicate of an account as a normal user.
         """
-        response = client.post(list_url, existing_acc_data, **normal_user_auth)
+        response = client.post(list_url, league_acc.existing_acc_data, **normal_user_auth)
         assert response.status_code == 400
         error_message = 'The fields username, server must make a unique set.'
         assert error_message in response.data['non_field_errors']
