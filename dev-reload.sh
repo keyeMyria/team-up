44ae8eacc789ace9ed5c23b26fe1d0dd49e9e9c4
Jakub Semik <kuba.semik@gmail.com>
2017-09-18 20:10:20 +0200

    Check if inotifywait is installed

--- dev-reload.sh
+++ dev-reload.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-inotifywait -e close_write -m -r . --exclude '(.git|.idea|node_modules)'|
-while read -r directory events filename; do
-  if [[ "$directory" = ./nginx/* ]]; then
-    echo "Reloading Nginx"
-    make reload-nginx &
-  fi
-done
+if hash inotifywait 2>/dev/null; then
+    inotifywait -e close_write -m -r . --exclude '(.git|.idea|node_modules)'|
+    while read -r directory events filename; do
+      if [[ "$directory" = ./nginx/* ]]; then
+        echo "Reloading Nginx"
+        make reload-nginx &
+      fi
+    done
+else
+    echo -e "\033[0;31mPlease run 'sudo apt install inotifywait' first!"
+    exit 1
+fi
+
+
+
