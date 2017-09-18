ab07c20bd0f53454508b7f8a11f32606c3022708
Jakub Semik <kuba.semik@gmail.com>
2017-09-18 21:02:54 +0200

    fixed static url

--- server/src/config/settings.py
+++ server/src/config/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/rest_framework/'
 
 INTERNAL_IPS = ['*']
 
