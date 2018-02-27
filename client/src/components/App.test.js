1b27ecedc3be02661b568008a822581fa063d8aa
Radek Krzak <radek.krzak@gmail.com>
2018-02-27 21:44:39 +0100

    Fix App.test.js, install some packages
    * add enzyme, redux-mock-store, redux-form

--- client/src/components/App.test.js
+++ client/src/components/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ConnectedRouter } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import createHistory from 'history/createBrowserHistory';
+import configureStore from 'redux-mock-store';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const mockStore = configureStore([]);
+const store = mockStore({});
+const history = createHistory();
+
+describe('<App />', () => {
+  const ConnectedApp = () => (
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </Provider>
+  );
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<ConnectedApp />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
