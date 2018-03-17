61032c323551248a524e2ae6eb5f6d20907be6c7
Jakub Semik <kuba.semik@gmail.com>
2018-03-17 09:35:58 +0100

    App layout (#15)
    * app layout
    
    * app layout
    
    * fixed indentation
    
    * added newline

--- client/src/views/Layout/index.js
+++ client/src/views/Layout/index.js
@@ -1,15 +1,25 @@
-import React, { Component, Fragment } from 'react';
+import React, {Component, Fragment} from 'react';
+import classNames from 'classnames/bind';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import styles from './styles.css';
+
+const cx = classNames.bind(styles);
 
 class Layout extends Component {
   render() {
     return (
       <Fragment>
-        <Header />
-        <main>{this.props.children}</main>
-        <Footer />
+        <div className={cx('container')}>
+          <div className={cx('header')}>
+            <Header/>
+          </div>
+          <main className={cx('content')}>{this.props.children}</main>
+          <div className={cx('footer')}>
+            <Footer/>
+          </div>
+        </div>
       </Fragment>
     );
   }
