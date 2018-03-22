60262377120b845eb0ebd1f06b8b45d049ca5d0f
radzak <radek.krzak@gmail.com>
2018-03-22 20:16:19 +0100

    Hardcode client_id and client_secret for dev purposes
    * hardcode oauth2 app client_id and client_secret
    * remove /server prefix for .env.development

--- server/src/common/management/commands/create_oauth_app.py
+++ server/src/common/management/commands/create_oauth_app.py
@@ -13,4 +13,14 @@ class Command(BaseCommand):
                 name='Team-up',
                 authorization_grant_type='password',
                 client_type='confidential',
-                user_id=uid)
+                user_id=uid,
+
+                # hardcoded for development
+                client_id='za5Co8eueO0OgV9hXhIbbOWGVShpOJYB3Jgnia5z',
+                client_secret=(
+                    '0FK0oQQJu1TAlcJ1ZhOPgk3en0KLJdB8'
+                    'pgaqnAMovz52FJxjdwpFmU1Qt1dUtFEQ'
+                    'QFEYI4frmj7gCvfRBAff2ll9h7hf1aEB'
+                    'BC4Wvtwjb2HBuBOefNZBLJAZhsfDDOEm'
+                )
+            )
